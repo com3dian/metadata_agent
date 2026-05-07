9d35a88dcfb9ab1af50b182736766118c2055797
Na Li <na.li@surf.nl>
2026-05-07 15:55:34 +0200

    Use model from .env

--- examples/completion.py
+++ examples/completion.py
@@ -8,14 +8,18 @@ load_dotenv()
 api_key = os.getenv("SURF_API_KEY")
 headers = {"X-API-KEY": api_key, "Content-Type": "application/json"}
 willma_base_url = os.getenv("SURF_API_BASE")
+model = os.getenv("LLM_MODEL")
+
+print(f"----------- Using model: {model} -----------\n")
 
 response = requests.post(
     f"{willma_base_url}/chat/completions",
     data=json.dumps({
-        "model": "Qwen 2.5 Coder 32B Instruct AWQ",
+        "model": model,
         "messages": [{"role": "user", "content": "Hello! What can you do?"}],
     }),
     headers=headers
 ).json()
 
 print(response["choices"][0]["message"]["content"])
+print("\n----------- Done -----------")
